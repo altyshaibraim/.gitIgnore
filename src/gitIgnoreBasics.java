a6594b26677c3d19dd0f6818ddc56a4d797ee9f8
altyshaibraim <altyshaibraim@gmail.com>
2020-11-13 21:19:36 -0600

    created file & modified it enabled .gitignored

---
+++ src/gitIgnoreBasics.java
@@ -0,0 +1,12 @@
+public class gitIgnoreBasics {
+    public static void main(String[] args) {
+        System.out.println("                      To enable ignore functions on file or folder:");
+        System.out.println("right click on selected file or folder -> Git -> Add to .gitignore -> Add to .gitignore -> Add");
+        System.out.println("!!! After you enabled ignore function, there is a new file created .gitignore.");
+        System.out.println("    It contains the list of files that are ignored.");
+        System.out.println("                      To manually enable ignore function after .gitignore file was created");
+        System.out.println("in .gitignore file type /file name/");
+        System.out.println("!!! On IntelliJ color of a file or folder means following:");
+        System.out.println("                green -> untracked; beige -> ignored");
+    }
+}
