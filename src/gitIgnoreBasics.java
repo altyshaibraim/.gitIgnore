62ac6b76c9cfd7b2c0f71f6eeb4c2c362534b88d
altyshaibraim <altyshaibraim@gmail.com>
2020-11-14 00:45:32 -0600

    changed the structure

--- src/gitIgnoreBasics.java
+++ src/gitIgnoreBasics.java
@@ -1,14 +1,18 @@
 public class gitIgnoreBasics {
     public static void main(String[] args) {
-        System.out.println("                      To enable ignore functions on file or folder:");
-        System.out.println("right click on selected file or folder -> Git -> Add to .gitignore -> Add to .gitignore -> Add");
-        System.out.println("!!! After you enabled ignore function, there is a new file created .gitignore.");
-        System.out.println("    It contains the list of files that are ignored.");
-        System.out.println("                      To manually enable ignore function after .gitignore file was created");
-        System.out.println("in .gitignore file type /file name/");
-        System.out.println("!!! On IntelliJ color of a file or folder means following:");
-        System.out.println("                green -> untracked; beige -> ignored");
-        System.out.println("                      To unastage the file/folder OR to fix ignored file/folder:");
-        System.out.println("Commit -> check tracked file/folder OR uncheck ignored file/folder -> right click on selected file/folder -> Rollback ");
+        /*                     To enable ignore functions on file or folder:
+        right click on selected file or folder -> Git -> Add to .gitignore -> Add to .gitignore -> Add
+        !!! After you enabled ignore function, there is a new file created .gitignore. It contains the list of files
+                                                                                                   that are ignored.
+
+                               To manually enable ignore function after .gitignore file was created:
+        In .gitignore file type /file name/
+        !!! On IntelliJ color of a file or folder means following:
+                                                        green -> untracked; beige -> ignored
+
+                               To unastage the file/folder OR to fix ignored file/folder:
+        Commit -> check tracked file/folder OR uncheck ignored file/folder -> right click on selected file/folder ->
+                                                                                                         -> Rollback
+         */
     }
 }
