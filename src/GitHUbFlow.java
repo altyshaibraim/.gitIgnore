833692fd85651313db49256933d6e49fa6f31ddc
altyshaibraim <altyshaibraim@gmail.com>
2020-11-15 17:50:02 -0600

    added info about GitHub Flow

---
+++ src/GitHUbFlow.java
@@ -0,0 +1,14 @@
+public class GitHUbFlow {
+    public static void main(String[] args) {
+    /*                     The main concepts behind the Github flow are:
+
+    -Anything in the master branch is deployable.
+    -To work on something new, create a descriptively named branch off of master (feature branches)
+    -Commit to that branch locally and regularly push your work to remote the same named branch.
+    -When you need feedback or help, or you think the branch is ready for merging, open a pull request.
+    -After someone else has reviewed and signed off on the feature, you can merge it into master.
+    -Once it is merged and pushed to ‘master’, you can and should deploy immediately.
+
+       */
+    }
+}
